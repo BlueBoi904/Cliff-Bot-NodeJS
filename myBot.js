dda2e44e88c96997031829a2ac4584d4cbc4048e
Benji Mirschel <cliff.mirschel@gmail.com>
2019-08-01 10:17:33 -0400

    Listing out connect channels

--- myBot.js
+++ myBot.js
@@ -4,7 +4,17 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
   console.log("Connected as " + client.user.tag);
+  // After connecting, update bots status to show it's currently playing a game
+  client.user.setActivity("with Javascript");
+
+  client.guilds.forEach(guild => {
+    console.log(guild.name);
+    guild.channels.forEach(channel => {
+      console.log(` - ${channel.name} ${channel.type} ${channel.id}`);
+    });
+    // General channel id: 216246550253273089
+  });
 });
 
-client.login("NjA2NDgzNTUwNDE3NDUzMDY2.XULuKg.7VTfHhR3lftMRkRW2nzO2MmaImM");
+client.login("NjA2NDgzNTUwNDE3NDUzMDY2.XULxog.qRzZEQOhpmpY1yj8a94s1Fo2nGs");
 // Bot is now online
